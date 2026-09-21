c419f383096ec07588ed856a62e753baa75c63db
MicroChip818 <ekhoock10@gmail.com>
2026-09-20 21:33:18 -0700

    Improved comment readability and added comments for loops

--- src/main.cpp
+++ src/main.cpp
@@ -9,11 +9,11 @@ void setup() {
 }
 
 void loop() {
-  int brightness = analogRead(7) / 16; // Brightness ranges from 0 to 255 (8-bit analog)
-  int period = analogRead(6) / 4.1363 + 10; // Period ranges from 10 ms to 1000 ms
-  int color = analogRead(5) / 819; // Color ranges from 0 (all off) to 5 (all on)
+  int brightness = analogRead(7) / 16; // Brightness range: 0-255 (8-bit PWM value)
+  int period = analogRead(6) / 4.1363 + 10; // Blink period: 10-1000 (ms)
+  int color = analogRead(5) / 819; // Color range: 0-5 (# of LEDs turned on at a time)
 
-  for (int i = 1; i <= 5; i++) {
+  for (int i = 1; i <= 5; i++) { // More pins will be lighted up with a higher color value
     if (color >= i) {
       analogWrite(i + 7, brightness);
     }
@@ -21,7 +21,7 @@ void loop() {
 
   delay(period);
 
-  for (int i = 12; i >= 8; i--) {
+  for (int i = 12; i >= 8; i--) { // Turns off all LEDs regardless of color value
     analogWrite(i, 0);
   }
 
